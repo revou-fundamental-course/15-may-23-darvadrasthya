b6e5a93ea19d5942a4359543850556d74c93dcf5
darva drasthya <ddarfa24@gmail.com>
2023-05-20 02:00:35 +0700

    setting js visible

--- .github/js/script.js
+++ .github/js/script.js
@@ -1,10 +1,13 @@
 function hitungLuas(){ 
     var sisiLuas = document.getElementById("sisi-luas").value;
 
-    document.getElementsByClassName("result-luas").style="visibility";
-
-    console.log(sisiLuas); 
+    list = document.getElementsByClassName("result-luas");
+    document.getElementsByClassName("result-luas")
+    if (list) {
+        list[x].style.visibility = "hidden";
+    }
+    
+    //document.getElementsByClassName("result-luas").style.visibility = "hidden";
     document.getElementById("ouput-luas").innerHTML = '<span>'+sisiLuas+'</span>';
-
     
 }
