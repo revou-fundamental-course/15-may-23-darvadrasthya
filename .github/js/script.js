6f74a9f3984c258109a3a6283612b63ab581e455
darva drasthya <ddarfa24@gmail.com>
2023-05-19 23:21:37 +0700

    add some javascript

---
+++ .github/js/script.js
@@ -0,0 +1,8 @@
+function hitungLuas(){ 
+    var sisiLuas = document.getElementById("sisi-luas").value;
+
+    document.getElementsByClassName("result-luas").style="visibility";
+
+    console.log(sisiLuas); 
+    document.getElementById("ouput-luas").innerHTML = '<span>'+sisiLuas+'</span>';
+}
