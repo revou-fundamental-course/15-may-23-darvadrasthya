d63eb8412b26147d796c173d372c836ebf471750
darva drasthya <ddarfa24@gmail.com>
2023-05-23 17:27:22 +0700

    Tambah keliling persegi

--- .github/main/script.js
+++ .github/main/script.js
@@ -18,7 +18,31 @@ function hitungLuas(){
   }
   function resetLuas() {
     document.getElementById("sisi-luas").value = "";
-    document.getElementById("rumus-luas").textContent = "";
+    document.getElementById("rumus-luas").style.visibility = "";
     document.getElementById("perhitungan-luas").textContent = "";
     document.getElementById("output-luas").textContent = "";
   }
+  function hitungKeliling(){
+    let kelilingPersegi = document.getElementById("keliling-persegi").value;
+    var hitung = 4 * kelilingPersegi;  
+
+    document.getElementById("rumus-keliling").style.visibility = "visible";
+    document.getElementById("rumus-keliling").style.visibility = "hidden";
+
+    var element = document.getElementById("rumus-keliling");
+      if (element.style.visibility === "hidden") {
+        element.style.visibility = "visible";
+      } else {
+        element.style.visibility = "hidden";
+    }
+
+    document.getElementById("perhitungan-keliling").textContent = "L = "+ kelilingPersegi + "x" +kelilingPersegi;  
+    // Display the result
+    document.getElementById("output-keliling").textContent = "K = "+ hitung;
+}
+function resetKeliling() {
+  document.getElementById("keliling-persegi").value = "";
+  document.getElementById("rumus-keliling").style.visibility = "";
+  document.getElementById("perhitungan-keliling").textContent = "";
+  document.getElementById("output-keliling").textContent = "";
+}
